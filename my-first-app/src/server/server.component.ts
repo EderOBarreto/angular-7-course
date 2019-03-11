bd3db4cc57b990988df79d535d3904f7724ce8c5
Eder <ederoliveirabarreto@gmail.com>
2019-03-11 18:11:15 -0300

    My-First-App

---
+++ my-first-app/src/server/server.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    //the selector is used to specify the HTML tag that will be used
+    selector: 'app-server',
+    // the templateUrl is used to specify the url of the HTML file that will be loaded
+    templateUrl: './server.component.html'
+    //
+    
+})
+export class ServerComponent { 
+
+}
