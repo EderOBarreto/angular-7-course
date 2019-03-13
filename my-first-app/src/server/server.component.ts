c0e1a7ec4f4195aff763c176131bd098bcce8f26
Eder <ederoliveirabarreto@gmail.com>
2019-03-13 10:16:22 -0300

    Some examples of built-in Directives

--- my-first-app/src/server/server.component.ts
+++ my-first-app/src/server/server.component.ts
@@ -1,19 +1,31 @@
 import { Component } from '@angular/core';
+import { CloneVisitor } from '@angular/compiler/src/i18n/i18n_ast';
 
 @Component({
     //the selector is used to specify the HTML tag that will be used
     selector: 'app-server',
     // the templateUrl is used to specify the url of the HTML file that will be loaded
-    templateUrl: './server.component.html'
-    //
-    
+    templateUrl: './server.component.html',
+    styles:[`
+        .online{
+            color: white;
+        }
+    `]
 })
 
 export class ServerComponent { 
     serverId : number = 10;
     serverStatus : string = 'offline';
 
+    constructor(){
+        this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline'; 
+    }
+
     getServerStatus(){
-        return 'offline';
+        return this.serverStatus;
+    }
+
+    getcolor(){
+        return this.serverStatus === 'online' ? '#0F0' :'#F00';
     }
 }
